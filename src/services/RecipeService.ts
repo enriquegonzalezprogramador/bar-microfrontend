17366b70255d254ab1499a5deed80071f3151487
Enrique Gonzalez Montes Junior <twd00026@gmail.com>
2024-05-25 19:57:38 -0300

    first commit

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -2,8 +2,17 @@ import axios from 'axios'
 import { CategoriesAPIResponseSchema, DrinksAPIResponse, RecipeAPIResponseSchema } from '../utils/recipes-schema'
 import { Drink, SearchFilter } from '../types'
 
+
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error('VITE_API_BASE_URL no está definida');
+}
+
+
 export async function getCategories() {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    const url = `${API_BASE_URL}list.php?c=list`;
     const { data } = await axios(url)
     const result = CategoriesAPIResponseSchema.safeParse(data)
     if(result.success) {
@@ -12,7 +21,7 @@ export async function getCategories() {
 }
 
 export async function getRecipes(filters: SearchFilter) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`
+    const url = `${API_BASE_URL}filter.php?c=${filters.category}&i=${filters.ingredient}`;
     const { data } = await axios(url)
     const result = DrinksAPIResponse.safeParse(data)
     if(result.success){
@@ -21,7 +30,7 @@ export async function getRecipes(filters: SearchFilter) {
 }
 
 export async function getRecipeById(id: Drink['idDrink']) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    const url = `${API_BASE_URL}lookup.php?i=${id}`;
     const { data } = await axios(url)
     const result = RecipeAPIResponseSchema.safeParse(data.drinks[0])
     if(result.success) {
